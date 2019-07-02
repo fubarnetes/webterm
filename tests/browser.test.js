f12153ea8f5175af834eee9f3c52df757b2deec7
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2019-07-02 02:59:41 +0200

    more tests

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -32,6 +32,72 @@ describe('Basic tests', () => {
         await expect(page.goto('http://localhost:8080')).resolves.not.toBeNull();
     });
 
+    it('can run echo command', async () => {
+        await page.goto('http://localhost:8080');
+        let content = await page.evaluate(async () => {
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+
+            sock.send('echo teststring\n');
+            await sleep(50);
+
+            term.selectAll();
+            return term.getSelection();
+        });
+        await expect(content).toMatch(/echo teststring\n/);
+        await expect(content).toMatch(/\nteststring\n/);
+    });
+
+    it('passes viewport to client software', async () => {
+        await page.goto('http://localhost:8080');
+        let viewport = await page.evaluate(() => {
+            return {
+                cols: term.cols,
+                rows: term.rows
+            }
+        });
+
+        expect(viewport.rows).toBeGreaterThan(0);
+        expect(viewport.cols).toBeGreaterThan(0);
+
+        let content = await page.evaluate(async () => {
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+
+            sock.send('stty size\n');
+            await sleep(50);
+
+            term.selectAll();
+            return term.getSelection();
+        });
+        expect(content).toContain(`${viewport.rows} ${viewport.cols}`);
+    });
+
+    it('sends resize events', async () => {
+        await page.goto('http://localhost:8080');
+        await page.evaluate(() => {
+            sock.send('trap \'stty size\' SIGWINCH\n');
+        });
+        await page.setViewport({width: 400, height: 600});
+        let content = await page.evaluate(async () => {
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+            await sleep(50);
+            term.selectAll();
+            return term.getSelection();
+        });
+        let viewport = await page.evaluate(() => {
+            return {
+                cols: term.cols,
+                rows: term.rows
+            }
+        });
+        expect(content).toContain(`${viewport.rows} ${viewport.cols}`);
+    })
+
     afterAll(async () => {
         return new Promise((resolve, reject) => {
             server_process.on('exit', (code, signal) => {
